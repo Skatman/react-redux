fbd6893aa7d70ad5b3cac985df07fa093fe2f6ed
Andrey Shynkarenko <andreyshynkarenko@Andreys-MacBook-Air.local>
2017-08-28 15:07:10 +0300

    fix of the previous commit

---
+++ WeatherApp/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'f3a856497ee3c5164c3c4b7b01a17cf9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
